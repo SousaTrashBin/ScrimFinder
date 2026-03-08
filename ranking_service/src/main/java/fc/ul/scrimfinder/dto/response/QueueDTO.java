b5b29b526714056f5697ae304f62bbbb0a65b4c4
sousanamain <sousanamain@gmail.com>
2026-03-08 01:43:04 +0000

    add missing fields

--- ranking_service/src/main/java/fc/ul/scrimfinder/dto/response/QueueDTO.java
+++ ranking_service/src/main/java/fc/ul/scrimfinder/dto/response/QueueDTO.java
@@ -2,6 +2,11 @@ package fc.ul.scrimfinder.dto.response;
 
 import fc.ul.scrimfinder.util.MMRRuleType;
 
-public record QueueDTO(Long queueId, Integer initialMMR, MMRRuleType mmrRuleType) {
-
+public record QueueDTO(
+        Long id,
+        String name,
+        MMRRuleType mmrRuleType,
+        int initialMMR,
+        boolean active
+) {
 }
