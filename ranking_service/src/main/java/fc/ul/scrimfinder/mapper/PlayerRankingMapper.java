16dc1a4cd47173b9cd531129f20cc48c0dd1747e
sousanamain <sousanamain@gmail.com>
2026-03-08 01:41:45 +0000

    add appropriate mappings for playerRanking DTO

--- ranking_service/src/main/java/fc/ul/scrimfinder/mapper/PlayerRankingMapper.java
+++ ranking_service/src/main/java/fc/ul/scrimfinder/mapper/PlayerRankingMapper.java
@@ -3,10 +3,21 @@ package fc.ul.scrimfinder.mapper;
 import fc.ul.scrimfinder.domain.PlayerRanking;
 import fc.ul.scrimfinder.dto.response.PlayerRankingDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface PlayerRankingMapper {
+
+    @Mapping(source = "player.id", target = "playerId")
+    @Mapping(source = "player.discordUsername", target = "username")
+    @Mapping(source = "player.lolAccountPPUID", target = "lolAccountPPUID")
+    @Mapping(source = "queue.id", target = "queueId")
     PlayerRankingDTO toDTO(PlayerRanking playerRanking);
 
+    @Mapping(source = "playerId", target = "player.id")
+    @Mapping(source = "username", target = "player.discordUsername")
+    @Mapping(source = "lolAccountPPUID", target = "player.lolAccountPPUID")
+    @Mapping(source = "queueId", target = "queue.id")
+    @Mapping(target = "privateId", ignore = true)
     PlayerRanking toEntity(PlayerRankingDTO playerRankingDTO);
 }
