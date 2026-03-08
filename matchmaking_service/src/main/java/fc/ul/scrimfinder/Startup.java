b2dae35be935b5a749300152832ef00647d9b5a7
sousanamain <sousanamain@gmail.com>
2026-03-08 07:26:31 +0000

    add onStart event, on startup should have a global queue if it doesn't exist

---
+++ matchmaking_service/src/main/java/fc/ul/scrimfinder/Startup.java
@@ -0,0 +1,32 @@
+package fc.ul.scrimfinder;
+
+import fc.ul.scrimfinder.domain.Queue;
+import fc.ul.scrimfinder.repository.QueueRepository;
+import fc.ul.scrimfinder.util.MatchmakingMode;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
+public class Startup {
+
+    @Inject
+    QueueRepository queueRepository;
+
+    @Transactional
+    void onStart(@Observes StartupEvent ev) {
+        if (queueRepository.findByIdOptional(1L).isEmpty()) {
+            Queue globalQueue = new Queue();
+            globalQueue.setId(1L);
+            globalQueue.setName("Global Queue");
+            globalQueue.setNamespace("GLOBAL");
+            globalQueue.setRequiredPlayers(10);
+            globalQueue.setRoleQueue(false);
+            globalQueue.setMode(MatchmakingMode.RANK_BASED);
+            globalQueue.setMmrWindow(200);
+            queueRepository.persist(globalQueue);
+        }
+    }
+}
