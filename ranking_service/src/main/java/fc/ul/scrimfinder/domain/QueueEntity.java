67e3829c6b4485e7280e4ca0f5c3a04abb4d81dc
sousanamain <sousanamain@gmail.com>
2026-03-08 01:43:13 +0000

    add missing fields

--- ranking_service/src/main/java/fc/ul/scrimfinder/domain/QueueEntity.java
+++ ranking_service/src/main/java/fc/ul/scrimfinder/domain/QueueEntity.java
@@ -1,17 +1,31 @@
 package fc.ul.scrimfinder.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import fc.ul.scrimfinder.util.MMRRuleType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@Table(name = "queue")
 public class QueueEntity {
+
     @Id
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MMRRuleType mmrRuleType;
+
+    @Column(nullable = false)
+    private int initialMMR = 1000;
+
+    @Column(nullable = false)
+    private boolean active = true;
 
 }
