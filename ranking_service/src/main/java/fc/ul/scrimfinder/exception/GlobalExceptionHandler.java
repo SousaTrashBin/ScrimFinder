7f8999ac067fc9c10cf76db59252d7556d4f22c5
sousanamain <sousanamain@gmail.com>
2026-03-08 01:40:37 +0000

    improve exception handling and add new exception mappings

--- ranking_service/src/main/java/fc/ul/scrimfinder/exception/GlobalExceptionHandler.java
+++ ranking_service/src/main/java/fc/ul/scrimfinder/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package fc.ul.scrimfinder.exception;
 
+import fc.ul.scrimfinder.util.ErrorResponse;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestResponse;
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
@@ -7,17 +8,69 @@ import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
 public class GlobalExceptionHandler {
 
     @ServerExceptionMapper
-    public RestResponse<String> mapException(PlayerNotFoundException x) {
-        return RestResponse.status(Response.Status.NOT_FOUND, x.getMessage());
+    public RestResponse<ErrorResponse> mapException(PlayerNotFoundException x) {
+        return RestResponse.status(
+                Response.Status.NOT_FOUND,
+                new ErrorResponse("PLAYER_NOT_FOUND", x.getMessage())
+        );
     }
 
     @ServerExceptionMapper
-    public RestResponse<String> mapException(QueueNotFoundException x) {
-        return RestResponse.status(Response.Status.NOT_FOUND, x.getMessage());
+    public RestResponse<ErrorResponse> mapException(QueueNotFoundException x) {
+        return RestResponse.status(
+                Response.Status.NOT_FOUND,
+                new ErrorResponse("QUEUE_NOT_FOUND", x.getMessage())
+        );
     }
 
     @ServerExceptionMapper
-    public RestResponse<String> mapException(MMRAlreadyExistsException x) {
-        return RestResponse.status(Response.Status.CONFLICT, x.getMessage());
+    public RestResponse<ErrorResponse> mapException(MMRAlreadyExistsException x) {
+        return RestResponse.status(
+                Response.Status.CONFLICT,
+                new ErrorResponse("MMR_ALREADY_EXISTS", x.getMessage())
+        );
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ErrorResponse> mapException(PlayerAlreadyCreatedException x) {
+        return RestResponse.status(
+                Response.Status.CONFLICT,
+                new ErrorResponse("PLAYER_ALREADY_EXISTS", x.getMessage())
+        );
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ErrorResponse> mapException(ExternalAccountNotFoundException x) {
+        return RestResponse.status(
+                Response.Status.BAD_REQUEST,
+                new ErrorResponse("EXTERNAL_ACCOUNT_NOT_FOUND", x.getMessage())
+        );
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ErrorResponse> mapException(LeagueAccountNotLinkedException x) {
+        return RestResponse.status(
+                Response.Status.BAD_REQUEST,
+                new ErrorResponse("LEAGUE_ACCOUNT_NOT_LINKED", x.getMessage())
+        );
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ErrorResponse> mapException(ExternalServiceUnavailableException x) {
+        return RestResponse.status(
+                Response.Status.SERVICE_UNAVAILABLE,
+                new ErrorResponse("EXTERNAL_SERVICE_UNAVAILABLE", x.getMessage())
+        );
+    }
+
+    @ServerExceptionMapper
+    public RestResponse<ErrorResponse> mapException(Exception x) {
+        return RestResponse.status(
+                Response.Status.INTERNAL_SERVER_ERROR,
+                new ErrorResponse(
+                        "INTERNAL_SERVER_ERROR",
+                        "An unexpected error occurred. Please try again later."
+                )
+        );
     }
 }
