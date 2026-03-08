d787d73f4be396959e5a4650888cf8392967dcb3
sousanamain <sousanamain@gmail.com>
2026-03-08 01:46:11 +0000

    add tier/rank classes to represents a player's rank

---
+++ ranking_service/src/main/java/fc/ul/scrimfinder/dto/response/RankDTO.java
@@ -0,0 +1,6 @@
+package fc.ul.scrimfinder.dto.response;
+
+import fc.ul.scrimfinder.util.Tier;
+
+public record RankDTO(Tier tier, Integer division, Integer lps) {
+}
---
+++ ranking_service/src/main/java/fc/ul/scrimfinder/util/Tier.java
@@ -0,0 +1,14 @@
+package fc.ul.scrimfinder.util;
+
+public enum Tier {
+    IRON,
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM,
+    EMERALD,
+    DIAMOND,
+    MASTER,
+    GRANDMASTER,
+    CHALLENGER
+}
