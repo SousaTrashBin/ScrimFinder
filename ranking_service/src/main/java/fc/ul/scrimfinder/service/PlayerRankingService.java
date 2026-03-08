4bfb318a1682057d8f2b95e22908919fe8eac247
sousanamain <sousanamain@gmail.com>
2026-03-08 01:42:23 +0000

    fix method signatures, also add method to process match results

--- ranking_service/src/main/java/fc/ul/scrimfinder/service/PlayerRankingService.java
+++ ranking_service/src/main/java/fc/ul/scrimfinder/service/PlayerRankingService.java
@@ -1,22 +1,25 @@
 package fc.ul.scrimfinder.service;
 
 import fc.ul.scrimfinder.dto.request.CreatePlayerRequest;
-import fc.ul.scrimfinder.dto.request.UpdateMMRRequest;
+import fc.ul.scrimfinder.dto.request.MatchResultRequest;
 import fc.ul.scrimfinder.dto.response.PaginatedResponseDTO;
 import fc.ul.scrimfinder.dto.response.PlayerRankingDTO;
+import fc.ul.scrimfinder.exception.LeagueAccountNotLinkedException;
 import fc.ul.scrimfinder.exception.MMRAlreadyExistsException;
 import fc.ul.scrimfinder.exception.PlayerNotFoundException;
 import fc.ul.scrimfinder.exception.QueueNotFoundException;
 import jakarta.validation.Valid;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface PlayerRankingService {
-    PlayerRankingDTO getPlayerRanking(Long playerId, Optional<Long> queueId) throws PlayerNotFoundException, QueueNotFoundException;
+    List<PlayerRankingDTO> getPlayerRanking(Long playerId, Optional<Long> queueId) throws PlayerNotFoundException, QueueNotFoundException;
 
-    PlayerRankingDTO updatePlayerMMR(Long playerId, UpdateMMRRequest updateMMRRequest) throws PlayerNotFoundException, QueueNotFoundException;
+    Map<Long, PlayerRankingDTO> processMatchResults(@Valid MatchResultRequest matchResultRequest) throws PlayerNotFoundException, QueueNotFoundException;
 
     PaginatedResponseDTO<PlayerRankingDTO> getQueueLeaderboard(int page, int size, Optional<Long> queueId);
 
-    PlayerRankingDTO populatePlayerMMR(Long playerId, @Valid CreatePlayerRequest createPlayerRequest) throws MMRAlreadyExistsException, QueueNotFoundException;
+    PlayerRankingDTO populatePlayerMMR(Long playerId, @Valid CreatePlayerRequest createPlayerRequest) throws MMRAlreadyExistsException, QueueNotFoundException, LeagueAccountNotLinkedException;
 }
