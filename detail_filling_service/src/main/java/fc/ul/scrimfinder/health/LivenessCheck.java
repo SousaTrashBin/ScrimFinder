c5199be463c3625a59093ed03f0943e18f8feef4
Bruno Faustino <bruno.faustino5000@gmail.com>
2026-03-12 03:53:43 +0000

    Add health checks for both liveness and readiness (database and Riot API availability - the last only with the skeleton for now). Add shutdown delay so the services can finish answering to their requests, but failing the readiness check for more requests

---
+++ detail_filling_service/src/main/java/fc/ul/scrimfinder/health/LivenessCheck.java
@@ -0,0 +1,17 @@
+package fc.ul.scrimfinder.health;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("LivenessCheck")
+                .up()
+                .build();
+    }
+}
---
+++ detail_filling_service/src/main/java/fc/ul/scrimfinder/health/ReadinessRiotApiCheck.java
@@ -0,0 +1,28 @@
+package fc.ul.scrimfinder.health;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+@Readiness
+@ApplicationScoped
+public class ReadinessRiotApiCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        if (isRiotApiAvailable()) {
+            return HealthCheckResponse.named("RiotApiAvailabilityCheck")
+                    .up()
+                    .build();
+        }
+        return HealthCheckResponse.named("RiotApiAvailabilityCheck")
+                .down()
+                .build();
+    }
+
+    private boolean isRiotApiAvailable() {
+        // TODO - Quick external endpoint check
+        return true;
+    }
+}
