0e4c5484eab17ccf633b029d0d63d802ff071ca8
sousanamain <sousanamain@gmail.com>
2026-03-19 20:30:35 +0000

    update, a queue is now based on region

--- matchmaking_service/src/main/java/fc/ul/scrimfinder/dto/response/QueueDTO.java
+++ matchmaking_service/src/main/java/fc/ul/scrimfinder/dto/response/QueueDTO.java
@@ -1,12 +1,14 @@
 package fc.ul.scrimfinder.dto.response;
 
 import fc.ul.scrimfinder.util.MatchmakingMode;
+import fc.ul.scrimfinder.util.Region;
 import lombok.Data;
 
 @Data
 public class QueueDTO {
     private Long id;
     private String name;
+    private Region region;
     private String namespace;
     private int requiredPlayers;
     private boolean isRoleQueue;
--- matchmaking_service/src/main/java/fc/ul/scrimfinder/service/QueueService.java
+++ matchmaking_service/src/main/java/fc/ul/scrimfinder/service/QueueService.java
@@ -2,9 +2,18 @@ package fc.ul.scrimfinder.service;
 
 import fc.ul.scrimfinder.dto.response.QueueDTO;
 import fc.ul.scrimfinder.util.MatchmakingMode;
+import fc.ul.scrimfinder.util.Region;
 
 public interface QueueService {
-    QueueDTO createQueue(Long id, String name, String namespace, int requiredPlayers, boolean isRoleQueue, MatchmakingMode mode, int mmrWindow);
+    QueueDTO createQueue(
+            Long id,
+            String name,
+            String namespace,
+            int requiredPlayers,
+            boolean isRoleQueue,
+            MatchmakingMode mode,
+            int mmrWindow,
+            Region region);
 
     QueueDTO getQueue(Long id);
 }
