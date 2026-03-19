07c365953c2eb5eeacd6a8b43f703c79510441fc
sousanamain <sousanamain@gmail.com>
2026-03-19 20:22:28 +0000

    remove json property (defaults to RED and BLUE)

--- match_history_service/src/main/java/fc/ul/scrimfinder/util/Role.java
+++ match_history_service/src/main/java/fc/ul/scrimfinder/util/Role.java
@@ -1,25 +1,15 @@
 package fc.ul.scrimfinder.util;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public enum Role {
-    @JsonProperty(value = "top")
     TOP("top"),
-
-    @JsonProperty(value = "jungle")
     JUNGLE("jungle"),
-
-    @JsonProperty(value = "mid")
     MID("mid"),
-
-    @JsonProperty(value = "bottom")
     BOTTOM("bottom"),
-
-    @JsonProperty(value = "support")
     SUPPORT("support");
 
     final String roleName;
