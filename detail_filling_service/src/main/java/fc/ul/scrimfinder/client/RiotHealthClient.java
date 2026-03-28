9db0f39612413c8848867b1d565cb6b46a394011
Bruno Faustino <bruno.faustino5000@gmail.com>
2026-03-28 03:39:05 +0000

    Add readiness health check for Riot's API in detail filling service. Rename custom health checks to maintain consistency with the built-in health checks from Quarkus

---
+++ detail_filling_service/src/main/java/fc/ul/scrimfinder/client/RiotHealthClient.java
@@ -0,0 +1,14 @@
+package fc.ul.scrimfinder.client;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(configKey = "riot-api")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface RiotHealthClient {
+    @HEAD
+    Response checkHealth();
+}
--- detail_filling_service/src/main/java/fc/ul/scrimfinder/health/LivenessCheck.java
+++ detail_filling_service/src/main/java/fc/ul/scrimfinder/health/LivenessCheck.java
@@ -10,6 +10,6 @@ import org.eclipse.microprofile.health.Liveness;
 public class LivenessCheck implements HealthCheck {
     @Override
     public HealthCheckResponse call() {
-        return HealthCheckResponse.named("LivenessCheck").up().build();
+        return HealthCheckResponse.named("Liveness check").up().build();
     }
 }
