dbebef817f0c07e48ffbbe290ab471d520cbcc49
Rodrigo-Neto-Dev <rodrigoptneto@gmail.com>
2026-06-18 11:56:20 +0100

    fixing bigquery tf location creation, in hopes to avoid recreation

--- infrastructure/terraform/bigquery.tf
+++ infrastructure/terraform/bigquery.tf
@@ -4,7 +4,7 @@
 resource "google_bigquery_dataset" "scrimfinder" {
   dataset_id    = "scrimfinder"
   project       = var.project_id
-  location      = var.region
+  location      = "EU"  # <-- FIXED: Match actual GCP location
   friendly_name = "ScrimFinder Main Database"
   description   = "Core ScrimFinder database for game and player data"
 
@@ -24,7 +24,7 @@ resource "google_bigquery_dataset" "scrimfinder" {
 resource "google_bigquery_dataset" "scrimfinder_platform" {
   dataset_id    = "scrimfinder_platform"
   project       = var.project_id
-  location      = var.region
+  location      = "EU"  # <-- FIXED: Match actual GCP location
   friendly_name = "ScrimFinder Platform Database"
   description   = "Platform-level data and analytics"
 
@@ -44,7 +44,7 @@ resource "google_bigquery_dataset" "scrimfinder_platform" {
 resource "google_bigquery_dataset" "ml_db" {
   dataset_id    = "ml_db"
   project       = var.project_id
-  location      = var.region
+  location      = "EU"  # <-- FIXED: Match actual GCP location
   friendly_name = "Machine Learning Database"
   description   = "ML models and training data"
 
